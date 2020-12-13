ba7e6903325e1ea8c78f6e9a94bf95c38b7f3129
Netanel <njamilis@fi.uba.ar>
2020-12-13 13:26:59 -0300

    Backend Proyecto

---
+++ CacProyectoPedidos/src/java/Persistencia.java
@@ -0,0 +1,14 @@
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Persistencia {
+    
+    private Connection cn;
+    private ResultSet rs;
+    private PreparedStatement ps;
+    private Statement sm;
+    
+}
